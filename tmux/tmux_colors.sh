c68bea3c6c725ce621ba2a307a53804004902c6a
zivoxRoot <theophiledefeche@ik.me>
2025-03-24 22:13:40 +0100

    add tmux

---
+++ tmux/tmux_colors.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# NOTE: This script loads the color from pywal16 and generate a color scheme for tmux
+
+# Load pywal colors
+[ -f "$HOME/.cache/wal/colors.sh" ] && . "$HOME/.cache/wal/colors.sh"
+
+# Generate the config
+cat <<EOF > "$HOME/.config/tmux/colors.conf"
+
+# Left side
+set -g status-left " \
+#[fg=color14]#[bg=color4,fg=color15] #S #[fg=color14,bg=color0] \
+ "
+
+# Right side
+set -g status-right "#[fg=color14,bg=color0]#[bg=color4,fg=color15] #(cat /sys/class/power_supply/BAT0/capacity)% #[fg=color14,bg=color0]\
+ #[fg=color14,bg=color4]#[bg=color4,fg=color15] %Y-%m-%d #[fg=color14,bg=color0]\
+ #[fg=color14,bg=color4]#[bg=color4,fg=color15] %H:%M #[fg=color14,bg=color0]"
+
+# Window styling
+setw -g window-status-format " \
+#[fg=color13]#[bg=color8,fg=color13] #I:#W #[fg=color13,bg=color0] \
+"
+setw -g window-status-current-format " \
+#[fg=color14]#[bg=color11,fg=color15] #I:#W #[fg=color14,bg=color0] \
+"
+setw -g window-status-style "bg=color0,fg=color14"
+setw -g window-status-current-style "bg=color0,fg=color14"
+EOF
