81e95854273e7e3c135dc873089cd08f355b9f95
zivoxRoot <theophiledefeche@ik.me>
2025-04-06 15:44:08 +0200

    hypr : update config

--- hypr/convert.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-for filename in $(\ls ./images/*.jpg 2>/dev/null | xargs -n1 basename); do
-	magick ./images/$filename -strip -thumbnail 500x500^ -gravity center -extent 500x500 ./cache/$filename
-done
--- hypr/scripts/hyprsunset.sh
+++ hypr/scripts/hyprsunset.sh
@@ -1,12 +1,10 @@
-#/bin/sh
+#/bin/bash
 
 # Check if hyprsunset is running
 if pgrep -x "hyprsunset" > /dev/null; then
-	# If running, kill it
 	pkill -x "hyprsunset"
 	notify-send "Hyprsunset deactivated"
 else
-	# If not running, start it
 	hyprsunset &
 	notify-send "Hyprsunset activated"
 fi
