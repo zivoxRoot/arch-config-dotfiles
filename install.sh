2ae195c61a62acb5b40a28abfefb79abfb3329ee
zivoxRoot <theophiledefeche@ik.me>
2025-05-18 20:12:29 +0200

    update

--- install.sh
+++ install.sh
@@ -145,6 +145,7 @@ copy_dir "$WALLPAPERS_SRC" "$WALLPAPERS_DEST" "Wallpapers"
 copy_dir "$WAL_SRC" "$WAL_DEST" "wal"
 
 # Create first wallpaper
+swww-daemon # Start the swww daemon
 first_wallpaper=$(ls -1 $WALLPAPER_DEST | head -n 1)
 swww img "$WALLPAPER_DEST"/"$first_wallpaper"
 wal -i "$WALLPAPER_DEST"/"$first_wallpaper" -n --cols16
